382310129b3506bbf26992882271b2f221baafd3
vladuhalys <vlad.lysenko1999@gmail.com>
2024-03-09 20:27:38 +0200

    Add new code

--- main.cpp
+++ main.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main() {
 	
+	
+	int start = 0;
+	int end = 100;
+	int i = start;
+	do
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << " ";
+		}
+		i++;
+	} while (i <= end);
+	cout << endl;
 	return 0;
 }
